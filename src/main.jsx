9c3c9b84c36eebd59ea5bbccb213958ae5f368ce
Gabriel Vazquez <gvazq82@gmail.com>
2024-09-04 14:44:39 -0400

    Initial Commit for React Router Changes

---
+++ src/main.jsx
@@ -0,0 +1,62 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Root, { 
+  loader as rootLoader, 
+  action as rootAction
+} from './routes/root';
+import Contact, {
+  loader as contactLoader,
+  action as contactAction,
+} from './routes/contact';
+import EditContact, { action as editAction } from './routes/edit';
+import { action as destroyAction } from './routes/destroy';
+import Index from './routes/index';
+import ErrorPage from "./error-page";
+
+import './index.css';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path= "/"
+      element={<Root />}
+      loader={rootLoader}
+      action={rootAction}
+      errorElement={<ErrorPage />}
+    >
+      <Route errorElement={<ErrorPage />}>
+        <Route index element={<Index />}/>
+        <Route 
+          path= "contacts/:contactId"
+          element={<Contact />}
+          loader={contactLoader}
+          action={contactAction}
+        />
+        <Route 
+          path= "contacts/:contactId/edit"
+          element={<EditContact />}
+          loader={contactLoader}
+          action={editAction}
+        />
+        <Route
+          path= "contacts/:contactId/destroy"
+          action={destroyAction}
+          errorElement={<div>Oops! There was an error.</div>}
+        />
+      </Route>
+    </Route>
+  ));
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider
+      router={router}
+    />
+  </StrictMode>,
+)
